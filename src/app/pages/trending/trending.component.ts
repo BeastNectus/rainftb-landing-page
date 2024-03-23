4bd62c63dda3b0450d9084dc51febaf73403791f
John Mamanao <kingfrost1143@gmail.com>
2024-03-23 18:44:57 +0800

    fixed responsive in trending section

--- src/app/pages/trending/trending.component.ts
+++ src/app/pages/trending/trending.component.ts
@@ -9,6 +9,7 @@ import { Component, CUSTOM_ELEMENTS_SCHEMA, HostListener } from '@angular/core';
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class TrendingComponent {
+    // spaceBetween:number = 100;
     slidesPerView:number = 3;
     screenWidth!: number;
 
@@ -17,12 +18,15 @@ export class TrendingComponent {
         this.screenWidth = window.innerWidth;
         if(this.screenWidth >= 320 && this.screenWidth <= 480){
             this.slidesPerView = 1
+            // this.spaceBetween = 50
         }
         else if(this.screenWidth >= 480 && this.screenWidth <= 992){
             this.slidesPerView = 2
+            // this.spaceBetween = 70
         }
         else if(this.screenWidth >= 992 && this.screenWidth <= 1200){
             this.slidesPerView = 3
+            // this.spaceBetween = 100
         }
     }
 }
