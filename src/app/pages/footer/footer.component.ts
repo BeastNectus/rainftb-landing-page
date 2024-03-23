cdec17c51e177ce80a71d76df5db1b8843e78cd8
ayangabry <iangabrielagujitas@gmail.com>
2024-03-23 19:05:52 +0800

    added footer section

--- src/app/pages/footer/footer.component.ts
+++ src/app/pages/footer/footer.component.ts
@@ -1,12 +1,51 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
 
+  links = [
+    {
+      "title": "About Us",
+      "section": [
+        { "label": "Our Company" },
+        { "label": "Our Resources" },
+        { "label": "Good Marketpace" },
+        { "label": "High Auctions" }
+      ]
+    },
+    {
+      "title": "Our Marketplace",
+      "section": [
+        { "label": "NFT Service Markers" },
+        { "label": "Trading Source Cards" },
+        { "label": "Holly Rainbow Shot" },
+        { "label": "NFT Shoe Product" }
+      ]
+    },
+    {
+      "title": "NFT Services",
+      "section": [
+        { "label": "NFT Upgrade" },
+        { "label": "NFT Update" },
+        { "label": "NFT Blowing" },
+        { "label": "NFT Auction" }
+      ]
+    },
+    {
+      "title": "Helpful Links",
+      "section": [
+        { "label": "Payment Service" },
+        { "label": "Terms Condition" },
+        { "label": "Suggestions" },
+        { "label": "Legal Services" }
+      ]
+    }
+  ]
 }
